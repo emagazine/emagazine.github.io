119a203a00e0a1b9b1f0b5076046a7e9072cb193
minhquang297 <minhquang2971999@gmail.com>
2021-01-05 22:23:15 +0700

    ver1

---
+++ index.js
@@ -0,0 +1,127 @@
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    console.log(scroll)
+});
+
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 410) {
+        $(".text1").removeClass("text1").addClass("text1_show");
+    } if (scroll <= 250) {
+        $(".text1_show").removeClass("text1_show").addClass("text1");
+    }
+});
+
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 1454) {
+        $(".talk11").removeClass("talk11").addClass("talk11_show");
+    } if (scroll <= 1450) {
+        $(".talk11_show").removeClass("talk11_show").addClass("talk11");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 1600) {
+        $(".talk12").removeClass("talk12").addClass("talk12_show");
+    } if (scroll <= 1550) {
+        $(".talk12_show").removeClass("talk12_show").addClass("talk12");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 1800) {
+        $(".talk13").removeClass("talk13").addClass("talk13_show");
+    } if (scroll <= 1780) {
+        $(".talk13_show").removeClass("talk13_show").addClass("talk13");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 2599) {
+        $(".wave_text1").removeClass("wave_text1").addClass("wave_text1_show");
+    } if (scroll <= 2589) {
+        $(".wave_text1_show").removeClass("wave_text1_show").addClass("wave_text1");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 2817) {
+        $(".wave_text2").removeClass("wave_text2").addClass("wave_text2_show");
+    } if (scroll <= 2810) {
+        $(".wave_text2_show").removeClass("wave_text2_show").addClass("wave_text2");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 4601) {
+        $(".textbox1").removeClass("textbox1").addClass("textbox1_show");
+    } if (scroll <= 4600) {
+        $(".textbox1_show").removeClass("textbox1_show").addClass("textbox1");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 4855) {
+        $(".textbox2").removeClass("textbox2").addClass("textbox2_show");
+    } if (scroll <= 4850) {
+        $(".textbox2_show").removeClass("textbox2_show").addClass("textbox2");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 5129) {
+        $(".textbox3").removeClass("textbox3").addClass("textbox3_show");
+    } if (scroll <= 5120) {
+        $(".textbox3_show").removeClass("textbox3_show").addClass("textbox3");
+    }
+});
+
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 7409) {
+        $(".qt1").removeClass("qt1").addClass("qt1_show");
+    } if (scroll <= 7400) {
+        $(".qt1_show").removeClass("qt1_show").addClass("qt1");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 7740) {
+        $(".qt2").removeClass("qt2").addClass("qt2_show");
+    } if (scroll <= 7730) {
+        $(".qt2_show").removeClass("qt2_show").addClass("qt2");
+    }
+});
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+
+    if (scroll >= 10004) {
+        $(".block5").removeClass("block5").addClass("block5_show");
+    } if (scroll <= 10000) {
+        $(".block5_show").removeClass("block5_show").addClass("block5");
+    }
+});
